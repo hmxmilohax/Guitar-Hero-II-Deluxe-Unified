ab8a535d4cca8bb1630af81923f7605ad4845ab5
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-12-27 21:39:38 -0600

    fix build scripts lawl

--- build_ps2.bat
+++ build_ps2.bat
@@ -1,2 +1,2 @@
-xcopy "%~dp0_ark\" "%~dp0_obj\ps2\" /exclude:"%~dp0dependencies\ps2_excludes.txt" /c /i /e /h /y
+xcopy "%~dp0_ark\" "%~dp0_obj\ps2\" /exclude:dependencies\ps2_excludes.txt /c /i /e /h /y
 "%~dp0dependencies\windows\arkhelper.exe" dir2ark "%~dp0_obj\ps2" "%~dp0_build\ps2\GEN" -n "MAIN" -s 4073741823
